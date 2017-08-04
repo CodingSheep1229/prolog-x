8e50d2351db95ef88eb003f529ae6bb4b5bab478
CodingSheep1229 <an4976887@gmail.com>
2017-08-05 01:37:40 +0800

    prolog3.0

--- application/views/main.php
+++ application/views/main.php
@@ -63,23 +63,30 @@ echo form_open('user/register' , $attributes);
 			<span class="col-md-2 col-sm-4 col-xs-4">
 				<span id="fixed-contact" class="bar-button">&emsp;&emsp;聯絡&emsp;&emsp;</span>
 			</span>
-		</div>	
+		</div>
 	</div>
 	<a id='intro' class="anchor" name="intro"></a>
 	<div id="intro-space">
 		<div id="intro-text">
 			<!--按下按鈕就插入文字-->
 		</div>
-		
-		<img id="book-btn" src="book.png" alt="理論上這裡要有一本書" >
-		
+		<img id="book-btn" src="book.png" alt="理論上這裡要有一本書">
 	</div>
-	<a id='schedule' class="anchor" name="schedule"></a>
+	<!-- 	<a id='schedule' class="anchor" name="schedule"></a>
 	<div class="container-fluid" id="schedule-space">
 		<div class="row" style="top: 9vw; position: relative;">
 			<div class="col-sm-4 col-xs-4"></div>
 			<img class="col-sm-6 col-xs-6" src="schedule.png" id="schedule-form">
 			<div class="col-sm-2 col-xs-2"></div>
+		</div>
+	</div> -->
+	<a id='schedule' class="anchor" name="schedule"></a>
+	<div id="schedule-space">
+		<div id="schedule-img-space">
+			<div style="padding-left: 2vw;">活動時間：8/29 ~ 8/31</div>
+			<a target="_blank" href="https://www.facebook.com/gocamping6/"><div style="padding-left: 2vw; margin-bottom: 1.5vw; color: #FFF0AC;">
+			活動地點：流星花園露營區</div></a>
+			<img src="schedule.png" id="schedule-form">
 		</div>
 	</div>
 	<div id="first-footer">
@@ -140,8 +147,69 @@ echo form_open('user/register' , $attributes);
 	<a id='register' class="anchor" name="register"></a>
 	<div id="register-space">
 		<div id='reg'>
-			<span>姓名：</span>
+			<div class="form-group">
+				<label>姓名：</label>
+				<input name="name" type="text" style="width:18vw;" maxlength="32" class="form-control input-block" placeholder="記得輸入本名rrrrr" />
+			</div>
+			<div class="form-group">
+				<label>出生年月日：</label>
+				<input name="birthday" type="date" style="width:11.75vw;" maxlength="32" class="form-control input-block" />
+			</div>
+			<div class="form-group">
+				<label>生理性別：
+					<input type="radio" class="form-check-input" name="gender" value="男"> 男
+					<input type="radio" class="form-check-input" name="gender" value="女"> 女
+				</label>
+			</div>
+			<div class="form-group">
+				<label>電子郵件：</label>
+				<input name="email" type="email" style="width:20vw;" maxlength="128" class="form-control input-block" placeholder="填學校的或自己的皆可" />
+			</div>
+			<div class="form-group">
+				<label>聯絡電話：</label>
+				<input name="phone" type="tel" style="width:20vw;" maxlength="32" class="form-control input-block" placeholder="Ex: 09XXXXXXXX" />
+			</div>
+			<div class="form-group">
+				<label>緊急聯絡人：</label>
+				<input name="emergency_contact" type="text" style="width:15vw;" maxlength="9" class="form-control input-block" placeholder="緊急聯絡人的名字" />
+			</div>
+			<div class="form-group">
+				<label>緊急聯絡人電話：</label>
+				<input name="emergency_phone" type="text" style="width:20vw;" maxlength="32" class="form-control input-block" placeholder="Ex: 09XXXXXXXX" />
+			</div>
+			<div class="form-group">
+				<label>與緊急聯絡人的關係：</label>
+				<input name="emergency_relation" type="text" style="width:18vw;" maxlength="9" class="form-control input-block" placeholder="Ex: 父子/母子/祖孫..." />
+			</div>
+			<div class="form-group">
+				<label>身分證字號：</label>
+				<input id="idd" name="id" type="text" style="width:25vw;" maxlength="32" class="form-control input-block" placeholder="Ex: 輸入你的身分證字號，保險用的" />
+			</div>
+			<div class="form-group">
+				<label>營服尺寸：</label>
+				<select class="form-control input-block" name="size" style="max-width: 10vw;">
+					<option value='XS'>XS</option>
+					<option value='S'>S</option>
+					<option value='M' SELECTED>M</option>
+					<option value='L'>L</option>
+					<option value='XL'>XL</option>
+					<option value='2L'>2L</option>
+					<option value='3L'>3L</option>
+				</select>
+			</div>
+			<div class="form-group">
+				<label>特殊食性：</label>
+				<input name="eat" type="text" style="width:25vw;" maxlength="32" class="form-control input-block" placeholder="有沒有特別不吃什麼的，沒有就填無" />
+			</div>
+			<div class="form-group">
+				<label>備註：</label>
+				<input name="other" type="text" style="width:30vw;" maxlength="128" class="form-control input-block" placeholder="有什麼想告訴我們的，或是哪幾天不能到都麻煩再這告訴我們：）" />
+			</div>
+			<button name="Submit" type="submit" value="報名" class="btn btn-primary btn-sm btn-xs">報名</button>
+
+			<!-- <span>姓名：</span>
 			<input name="name" type="text" style="width:18vw;" maxlength="32" placeholder="記得輸入本名rrrrr" />
+			<br>
 			<span>出生年月日:</span>
 			<input name="birthday" type="date" style="width:11.75vw;" maxlength="32" />
 			<br>
@@ -166,8 +234,8 @@ echo form_open('user/register' , $attributes);
 			<br>
 			<span>身分證字號:</span>
 			<input id='idd' name="id" type="text" style="width:25vw;" maxlength="32" placeholder="Ex: 輸入你的身分證字號，保險用的" />
-			<!-- <br> -->
-			<span style="margin-left: 3vw;">營服尺寸:</span>
+			<br>
+			<span>營服尺寸:</span>
 			<select name="size">
 				<option value='XS'>XS</option>
 				<option value='S'>S</option>
@@ -184,24 +252,20 @@ echo form_open('user/register' , $attributes);
 			<span>備註:</span>
 			<input name="other" type="text" style="width:50vw;" maxlength="128" placeholder="有什麼想告訴我們的，或是哪幾天不能到都麻煩再這告訴我們：）" />
 			<br>
-			<input id='sub' name="Submit" type="submit" value="報名" style="padding-top: 0vw;">
+			<input id='sub' name="Submit" type="submit" value="報名" style="padding-top: 0vw;"> -->
+
 		</div>
 	</div>
 	<a id="pay" class="anchor" name="pay"></a>
-	<div class="container-fluid" id="pay-space">
-		<div class="row" id="pay-text">
-			<div class="col-md-4 col-sm-4 col-xs-4"></div>
-			<div class="col-md-6 col-sm-6 col-xs-6">
-				<p>
-					<br>銀行代號：009
-					<br>帳號：6117-50-001813-00 潘美菁
-					<br>費用：3500元整
-					<br>報名與繳費期限：即日起至8/20
-					<br>匯款完成後請將匯款帳號後五碼寄到
-					<br>b04101049@NTUIMu.edu.tw 給我們做確認：）
-				</p>
-			</div>
-			<div class="col-md-2 col-sm-2 col-xs-2"></div>
+	<div id="pay-space">
+		<div id="pay-text">
+			銀行代號：700
+			<br>帳號：0001236-0541821
+			<br>戶名：國立臺灣大學資訊管理學系學生會林軒逸
+			<br>費用：3500元整
+			<br>報名與繳費期限：即日起至8/20
+			<br>匯款完成後請將匯款帳號後五碼寄到
+			<br>b04101049@NTUIMu.edu.tw 給我們做確認：）
 		</div>
 	</div>
 	<a id="game" class="anchor" name="game"></a>
